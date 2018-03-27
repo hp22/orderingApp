31ae8e07ab9a8395fdee378a26bbb3fc87588f08
Aram <aram.e@pg.com>
2018-03-27 08:21:31 +0100

    as

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -3,7 +3,7 @@ angular.module('mainApp', )
 
 .controller('loginCtrl', function($scope, $location){
     $scope.validate = function() {
-        if ($scope.usrname == "hem" && $scope.pwd == "123") {
+        if ($scope.usrname == "hem" && $scope.pwd == "asd") {
             alert("Wrong Username or Password!!");
         } else {
             $location.path('/seating');
--- controllers/seatCtrl.js
+++ controllers/seatCtrl.js
@@ -1,28 +1,31 @@
 angular.module('mainApp')
-    .controller("seatCtrl", function($scope) {
-        $scope.msg = 'hello';
-        $scope.tables = [];
-        class table {
-            constructor() {
-              this.table_no;
-                this.no_of_people = 1;
-                this.order = [];
-            }
-        }
+  .controller("seatCtrl", function ($scope) {
 
-        $scope.table1 = new table();
-        $scope.table2 = new table();
-        $scope.table3 = new table();
-        $scope.table4 = new table();
-        $scope.table5 = new table();
-
-        $scope.tables = [
-            $scope.table1,
-            $scope.table2,
-            $scope.table3,
-            $scope.table4,
-            $scope.table5,
-        ];
-      $scope.tables.splice(0,1);
+    $scope.seat1 = 'Seat 1';
+    $scope.seat2 = 'Seat 2';
+    $scope.seat3 = 'Seat 3';
+    $scope.seat4 = 'Seat 4';
+    $scope.tables = [];
+  
+    class table {
+      constructor(t_no, no) {
+        this.table_no = t_no;
+        this.no_of_people = no;
+        this.order = [];
+      }
+    }
+    $scope.addPeople = function (table_no) {
+      var no_of_people = Number(prompt('Please enter the no of people:'));
       
-    });
+      if (!isNaN(no_of_people) && no_of_people > 0) {
+        alert(table_no);
+      } else {
+        alert('error');
+      }
+      $scope.tables.push(new table(Number(table_no) ,no_of_people));
+      
+      document.write($scope.tables[0].no_of_people);
+
+    }
+
+  });
