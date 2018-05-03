bd62996179aba7716c4d183f39d35691a42dfa13
Hemanta Pun <hemantapun@MacBook-Pro.local>
2018-05-03 07:48:00 +0100

    books

--- order/app/scripts/services/itemsfactory.js
+++ order/app/scripts/services/itemsfactory.js
@@ -7,12 +7,6 @@ angular.module('orderApp')
     retObj.itemObject = {};
     retObj.flag = false;
 
-    var filenameList = [
-      "json-files/page1-items.json",
-      "json-files/page2-items.json",
-      "json-files/page3-items.json"
-    ];
-
     // Public API here
     retObj.getItems1 = function() {
       return ($http.get("json-files/page1-items.json"));
