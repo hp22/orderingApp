3ae3d49fb3683fbfe2d49db31160c71022589cd7
Aram <aram.e@pg.com>
2018-08-10 16:40:29 +0100

    css stuff, bootstrap theme

--- order/app/scripts/controllers/login.js
+++ order/app/scripts/controllers/login.js
@@ -3,20 +3,20 @@ angular.module('orderApp' )
 .controller('LoginCtrl', function($scope, $location, $http){
     $scope.validate = function() {
 
-        $http.get("demo_db_con.js").then(function (response){
-            console.log(data.records);
-        });
+//        $http.get("demo_db_con.js").then(function (response){
+//            console.log(data.records);
+//        });
 
-        if ($scope.usrname == "hem" && $scope.pwd == "asd") {
-            // alert("Wrong Username or Password!!");
-            $location.path('/seating');
+        if ($scope.usrname == "asd" && $scope.pwd == "asd") {
+             alert("Wrong Username or Password!!");
+//            $location.path('/seating');
 
         } else {
-            // $location.path('/seating');
-            console.log("here");
-            $http.get("./scripts/controllers/demo_db_con.js").then(function (response){
-                console.log(response.data);
-            });
+             $location.path('/seating');
+//            console.log("here");
+//            $http.get("./scripts/controllers/demo_db_con.js").then(function (response){
+//                console.log(response.data);
+//            });
             // alert("Wrong Username or Password!!");
 
         }
