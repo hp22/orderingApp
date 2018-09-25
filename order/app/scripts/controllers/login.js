f09cffa35584a0af878b3876fdffa6e1393ec4c9
Hemanta Pun <hempun22@gmail.com>
2018-09-25 22:28:26 +0100

    new

--- order/app/scripts/controllers/login.js
+++ order/app/scripts/controllers/login.js
@@ -1,22 +1,16 @@
 angular.module('orderApp')
 
   .controller('LoginCtrl', function ($scope, $location) {
-    $scope.user = "_";
-    $scope.var = 1;
     $scope.validate = function () {
-      $scope.var += 1;
 
 
-      if ($scope.usrname == "asd" && $scope.pwd == "asd") {
+      if ($scope.usrname == "hem" && $scope.pwd == "asd") {
 
         $location.path('/seating');
 
       } else {
-        $scope.user = "new";
-
-
-        
-        $location.path('/seating');
+          alert("invalid username or pwd");
+        // $location.path('/seating');
 
       }
     }
